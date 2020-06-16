22b102606bb4cf8cc6258df5d824d57b4b5decbc
vaandhare <vaibhavandhare7@gmail.com>
2020-06-16 14:21:21 +0530

    Updated androidTest Activity.

--- app/src/main/androidTest/com/udacity/gradle/builditbigger/EndpointAsyncTaskTest.java
+++ app/src/main/androidTest/com/udacity/gradle/builditbigger/EndpointAsyncTaskTest.java
@@ -1,5 +1,12 @@
 package com.udacity.gradle.builditbigger;
 
+import android.support.test.runner.AndroidJUnit4;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+import static org.junit.Assert.assertTrue;
+
 @RunWith(AndroidJUnit4.class)
 public class EndpointAsyncTaskTest {
     @Test
